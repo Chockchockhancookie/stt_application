48f257c920971e8ad9d3a4dcdd85b011c247b2cf
CHOCOChip_93 <mellow3632@naver.com>
2021-07-20 13:47:34 +0900

    0720-1

--- restfulapi/restfulapiserver/restfulapiserver/settings.py
+++ restfulapi/restfulapiserver/restfulapiserver/settings.py
@@ -25,8 +25,15 @@ SECRET_KEY = 'b*(7c^@ea)as3w^l5^%zd$#b=#pm^x9b1jpr5$)2%c_ocin3r-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Application definition
 
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
